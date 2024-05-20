ea79a91ac0c1fd0ffb66e9c377bb28f783419471
helioole <irarakovcena@gmail.com>
2024-05-21 00:10:50 +0300

    added axios

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,42 @@
-import React from "react";
-import Button from "@mui/material/Button";
+import React from 'react';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
-import styles from "./Header.module.scss";
-import Container from "@mui/material/Container";
+import styles from './Header.module.scss';
+import Container from '@mui/material/Container';
 
 export const Header = () => {
+  const isAuth = false;
+
+  const onClickLogout = () => {};
+
   return (
     <div className={styles.root}>
       <Container maxWidth="lg">
         <div className={styles.inner}>
-          <a className={styles.logo} href="/">
+          <Link className={styles.logo} to="/">
             <div>FAF BLOG</div>
-          </a>
+          </Link>
           <div className={styles.buttons}>
-            <Button variant="outlined">Sign In</Button>
-            <Button variant="contained">Sign Up</Button>
+            {isAuth ? (
+              <>
+                <Link to="/posts/create">
+                  <Button variant="contained">Write a new post</Button>
+                </Link>
+                <Button onClick={onClickLogout} variant="contained" color="error">
+                  Log Out
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <Button variant="outlined">Sign In</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Sign Up</Button>
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </Container>
