4417d1cce8cd351d430e2c1ecad304523d1a43cc
helioole <irarakovcena@gmail.com>
2024-05-21 02:06:11 +0300

    saving state status of request

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -1,4 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from '../../axios';
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async() =>{
+    const { data } = await axios.get('/posts');
+    return data;
+});
 
 
 const initialState ={
@@ -15,7 +21,22 @@ const initialState ={
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducer: {},
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchPosts.pending, (state) => {
+                state.posts.items = [];
+                state.posts.status = 'loading';
+            })
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                state.posts.items = action.payload;
+                state.posts.status = 'loaded';
+            })
+            .addCase(fetchPosts.rejected, (state) => {
+                state.posts.items = [];
+                state.posts.status = 'error';
+            });
+    },
 });
 
 export const postsReducer = postsSlice.reducer;
